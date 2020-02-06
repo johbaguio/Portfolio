2a5cff4b7bfa9cfe7873357e985e6e14352e03ec
Johanna <johbaguio@gmail.com>
2020-02-06 15:32:15 +0100

    Updated files

---
+++ src/Components/Main.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import LandingPage from './LandingPage';
+// import About from './AboutMe';
+// import Contact from './Contact';
+// import Projects from './Projects';
+// import Resume from './Resume';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+
+class Main extends Component {
+    render() {
+        return (
+            <Router>
+                <Header />
+                <Switch>
+                    <Route exact path='/' component={LandingPage} />
+                    {/* <Route exact path='/About' component={About} />
+                    <Route exact path='/Contact' component={Contact} />
+                    <Route exact path='/Projects' component={Projects} />
+                    <Route exact path='/Resume' component={Resume} /> */}
+                </Switch>
+            </Router>
+
+
+
+
+        )
+    }
+}
+
+
+export default Main;
