e55c0911b7d56f85062c5ac076814a43ab56512d
Johanna <johbaguio@gmail.com>
2020-02-08 16:00:17 +0100

    Updated files

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -13,13 +13,14 @@ const Header = () => {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav" >
                     <Nav className="mr-auto navtext">
+                        <NavLink to="/Home" className='navbar-text' activeClassName='current'>Home</NavLink>
                         <NavLink to="/About" className='navbar-text' activeClassName='current'>About</NavLink>
                         <NavLink to="/Resume" className='navbar-text' activeClassName='current'>Resume</NavLink>
-                        <NavLink to="/Projects" className='navbar-text' activeClassName='current'>Projects</NavLink>
                     </Nav>
                     <Nav>
+                        <NavLink to="/Projects" className='navbar-text' activeClassName='current'>Projects</NavLink>
                         <NavLink to="/Contact" className='navbar-text' activeClassName='current'>Contact</NavLink>
-                        <NavLink to="/Home" className='navbar-text' activeClassName='current'>Home</NavLink>
+
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
